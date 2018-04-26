8de92d196c6282f4eca84019a6392a29d8f6800d
Matt Ruffner <mattruffner7@gmail.com>
2018-04-26 13:51:01 -0400

    updated to no pid library, working

--- project/calibration/speed_fit.m
+++ project/calibration/speed_fit.m
@@ -1,4 +1,4 @@
-D=dlmread('speedcal-inc5.txt','\t');
+D=dlmread('speedcal2.tsv','\t');
 P=polyfit(D(:,1),D(:,2),3);
 figure(1)
 plot(D(:,1),D(:,2),D(:,1),polyval(P,D(:,1)))
@@ -7,3 +7,8 @@ pwmvals=linspace(81,255,175);
 lut=polyval(P,pwmvals);
 figure(2)
 plot(pwmvals,lut)
+
+fprintf("PROGMEM float RPMMap[175] = {");
+for n=1:175
+   fprintf("%d,\n",lut(n));
+end
